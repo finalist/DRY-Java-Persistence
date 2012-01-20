9e5eea8d276d56f84d13c714dd0a6460bd1de875
ernst-jan <ernst-jan@finalist.nl>
2012-01-20 16:34:31 +0100

    eerste testje erbij

---
+++ src/test/java/nl/finalist/dry/persistence/NotFoundExceptionTest.java
@@ -0,0 +1,12 @@
+package nl.finalist.dry.persistence;
+
+import org.junit.Test;
+
+public class NotFoundExceptionTest {
+
+    @Test
+    public void test() {
+        // fail("Not yet implemented");
+    }
+
+}
